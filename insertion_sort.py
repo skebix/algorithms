35eb1c8d3b691f6984661e7e57fe987d95263186
Néstor Manuel <nestor706@gmail.com>
2018-08-30 16:03:21 -0300

    A more straightforward insertion sort

--- insertion_sort.py
+++ insertion_sort.py
@@ -4,16 +4,11 @@ import random, time
 def insertion_sort(a):
     """Insertion sort algorithm. Take A[j], and insert it in the sorted sequence A[0..j-1]
     In any given iteration, A[0..j-1] is sorted and A[j+1..n] are the elements to be sorted"""
-
-    for j in range(1, len(a)):
-        current = a[j]
-
-        i = j - 1
-        while i >= 0 and a[i] > current:
-            a[i + 1] = a[i]
-            i -= 1
-
-        a[i + 1] = current
+    for i in range(1, len(a)):
+        j = i
+        while(j > 0 and l[j] < l[j - 1]):
+        	l[j], l[j - 1] = l[j - 1], l[j]
+        	j -= 1
 
 
 l = random.sample(range(1, 1000001), 10000)
