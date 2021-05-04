8fd860baa38e3caac673f551c8ade60cbf5e5875
Néstor Manuel <nestor706@gmail.com>
2021-05-04 17:36:23 -0300

    HackerRank "Sales by Match" from Interview Preparation Kit (Warm-up Challenges)

---
+++ sales_by_match.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+# HackerRank "Sales by Match" from Interview Preparation Kit (Warm-up Challenges)
+def sock_merchant(n, ar):
+    socks = {}
+    pairs = 0
+    for sock in ar:
+        if sock not in socks:
+            socks[sock] = sock
+        else:
+            pairs += 1
+            del socks[sock]
+    return pairs
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sock_merchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
